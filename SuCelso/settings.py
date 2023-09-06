c561f9d73d3b57a228e03f6f03ce23f656952794
EderNatan.dev <edernatan124@gmail.com>
2023-09-06 10:36:10 -0300

    static archives admin

--- SuCelso/settings.py
+++ SuCelso/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,6 +29,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+
+STATIC_URL_URL="/static/"
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 # Application definition
 
 INSTALLED_APPS = [
@@ -86,8 +92,10 @@ DATABASES = {
 
 
 
-
 '''erro aqui data bases'''
+
+
+
 '''
 DATABASES = {
     'default': {
