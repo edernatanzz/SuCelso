b67351b23b98c6ca43ce6e3dfd86b34e7452e7d3
EderNatan.dev <edernatan124@gmail.com>
2023-09-16 10:00:30 -0300

    login , p3

--- SuCelso/settings.py
+++ SuCelso/settings.py
@@ -149,4 +149,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #AUTH_USER_MODEL = 'student_management_app.CustomUser'
 AUTH_USER_MODEL = "student_management_app.CustomUser"
-
+AUTHENTICATION_BACKENDS = ['student_management_app.EmailBackEnd.EmailBackend']
--- student_management_app/views.py
+++ student_management_app/views.py
@@ -1,5 +1,41 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from student_management_app.EmailBackEnd import EmailBackend
 
+
+# ...
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method not allowed</h2>")
+    else:
+        username = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse(f"Email: {username}, Password: {password}")
+        else:
+            return HttpResponse("Invalid Login")
+
+#modelo 
+#def name(request):
+   # return render(request,".html")
 # Create your views here.
 def showDemoPage(request):
     return render(request,"demo.html")
+
+def ShowLoginPage(request):
+    return render(request,"login_page.html")
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+ request.user.email+"usertype : "+ request.user.user_type)
+    else:
+        return HttpResponse("Por favor faça login primeiro ")
+ 
+def logout_user(request):
+    logout(request) 
+    return HttpResponseRedirect("/")
